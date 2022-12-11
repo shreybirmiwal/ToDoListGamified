672c84235892bc2272d353723a2479271f42d731
Shrey <shreybirmiwal@gmail.com>
2022-12-11 12:08:38 -0600

    Working camera

--- components/takePicture.js
+++ components/takePicture.js
@@ -24,31 +24,38 @@ const TakePicture = () =>{
     } else if(!hasCameraPermission){
         return <Text> Permissions for camera not granted. Please change in settings.</Text>
     }
-    if(hasCameraPermission){
+
         return(
-            <View style={styles.container}>
-                <Text> has permissions</Text>
-                <Camera
-                ref={cameraRef}
-                captureAudio={false}
-                style={{flex: 1}}
-                ></Camera>
-            </View>
+            <Camera ref={cameraRef} style={styles.container}>
+                <Text style={styles.topText}> Take a picture of your completed task</Text>
+                <TouchableOpacity style={styles.picButton}/>
+            </Camera>
         )
-    }
+    
 
-    return(
-        <Camera ref={cameraRef}>
-        </Camera>
-
-    );
 }
 const styles = StyleSheet.create({
     container: {
         flex:1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        height: 500,
+        alignSelf: 'stretch'
     },
+    picButton:{
+        height: 120,
+        width: 120,
+        borderRadius: 1000,
+        backgroundColor: '#FFFF',
+        position: 'absolute',
+        bottom: 0,
+        marginBottom: 75,
+    },
+    topText:{
+        color: '#FFFF',
+        flex: 1,
+        marginTop: 100
+    }
 })
 
 
