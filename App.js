867f08549d1ac15768e394df7a8194f399873828
Shrey <shreybirmiwal@gmail.com>
2022-12-11 11:40:11 -0600

    continued camera

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
 import Task from './components/task';
 import Completed from './components/completed';
-import takePicture from './components/takePicture';
+import TakePicture from './components/takePicture';
 
 export default function App() {
   const [task, setTask] = useState();
@@ -35,55 +35,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <takePicture></takePicture>
-
-      <ScrollView
-        contentContainerStyle={{
-          flexGrow: 1
-        }}
-        keyboardShouldPersistTaps='handled'
-      >
-
-      <View style={styles.tasksWrapper}>
-        <Text style={styles.sectionTitle}>Today's tasks</Text>
-        <View style={styles.items}>
-          {
-            taskItems.map((item, index) => {
-              return (
-                <TouchableOpacity key={index}  onPress={() => completeTask(index)}>
-                  <Task text={item} /> 
-                </TouchableOpacity>
-              )
-            })
-            
-          }
-          {
-            finishedItems.map((item, index) => {
-              return (
-                <TouchableOpacity key={index}  onPress={() => DestroyTask(index)}>
-                  <Completed text={item} /> 
-                </TouchableOpacity>              )
-            })
-            
-          }
-        </View>
-      </View>
-        
-      </ScrollView>
-
-
-      <KeyboardAvoidingView 
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={styles.writeTaskWrapper}
-      >
-        <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)} />
-        <TouchableOpacity onPress={() => handleAddTask()}>
-          <View style={styles.addWrapper}>
-            <Text style={styles.addText}>+</Text>
-          </View>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
-      
+      <TakePicture></TakePicture>     
     </View>
   );
 }
--- components/takePicture.js
+++ components/takePicture.js
@@ -2,33 +2,55 @@ import {Camera} from 'expo-camera';
 import {shareAsync} from 'expo-sharing'
 import * as MediaLibrary from 'expo-media-library'
 import { useEffect, useRef, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
-export default function takePicture(){
+const TakePicture = () =>{
 
     let cameraRef = useRef();
     const [hasCameraPermission, setHasCameraPermission] = useState();
     const [hasMediaPermission, setHasMediaPermission] = useState();
 
     useEffect(() => {
-        (async () =>{
-            const cameraPermissions = await Camera.requestCameraPermissionsAsync();
-            const mediaLibraryPermissions = await MediaLibrary.requestPermissionsAsync();
-            setHasCameraPermission(cameraPermissions.status === "granted");
-            setHasMediaPermission(mediaLibraryPermissions.status === "granted");
-        })
-    }, [])
+        (async () => {
+          const cameraPermission = await Camera.requestCameraPermissionsAsync();
+          const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+          setHasCameraPermission(cameraPermission.status === "granted");
+          setHasMediaPermission(mediaLibraryPermission.status === "granted");
+        })();
+      }, []);
 
     if(hasCameraPermission === undefined){
         return <Text> Requesting Permissions</Text>
     } else if(!hasCameraPermission){
         return <Text> Permissions for camera not granted. Please change in settings.</Text>
     }
+    if(hasCameraPermission){
+        return(
+            <View style={styles.container}>
+                <Text> has permissions</Text>
+                <Camera
+                ref={cameraRef}
+                captureAudio={false}
+                style={{flex: 1}}
+                ></Camera>
+            </View>
+        )
+    }
 
     return(
-        <Camera>
-            
+        <Camera ref={cameraRef}>
         </Camera>
+
     );
-
-
 }
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+})
+
+
+
+export default TakePicture;
